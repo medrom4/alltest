861adf5a92417b377bfc152d7e0e03006e81a866
Ramanovich <medikroma@gmail.com>
2019-03-29 19:49:37 +0000

    console_log

--- SintacsisJS/script_my/script.js
+++ SintacsisJS/script_my/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+(function() {
+    var name = "Alex";
+
+    let surname = "Romanovich";
+
+    const sex = "Male";
+    
+    console.log("Имя: " + name + "\n" + "Фамилия: " + surname + "\n" + "Пол: " + sex);
+})();
