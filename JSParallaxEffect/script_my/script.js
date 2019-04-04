5981ef1432f4333d59152c2b553eb4ae7a6d423b
Ramanovich <medikroma@gmail.com>
2019-04-04 20:57:02 +0000

    Parallax-OK

--- JSParallaxEffect/script_my/script.js
+++ JSParallaxEffect/script_my/script.js
@@ -1,6 +1,2 @@
-$(window).scroll(function() {
-    var scroll_position = $(window).scrollTop()/2;
-    $('section').css({
-        'background-position-x': -scroll_position + 'px',
-    })
-})
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
