884457b1e21303013ca859e6e9f6deb656d45a57
Ramanovich <medikroma@gmail.com>
2019-04-16 20:26:24 +0000

    MenuCssJs-OK

---
+++ MenuJSCSS/script_my/script.js
@@ -0,0 +1,13 @@
+document.querySelector('.c-hamburger').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (this.classList.contains('is-active')) {
+        this.classList.remove('is-active');
+        document.querySelector('#menu').classList.remove('nav-active');
+        document.body.classList.remove('body-active');
+    } else {
+        this.classList.add('is-active');
+        document.querySelector('#menu').classList.add('nav-active');
+        document.body.classList.add('body-active');
+    }
+});
